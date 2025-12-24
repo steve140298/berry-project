3d2028fd123cada761fbadccf4b7eea8d6e0b67c
steve1402 <sleep1998@mail.ru>
2025-12-24 22:10:18 +0300

    Сделана шапка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,6 @@
 /* в этот файл добавляет скрипты*/
+let burgerButton = document.querySelector('.header__burger-button');
+let headerMenu = document.querySelector('.header__navigation')
+burgerButton.onclick = function() {
+  headerMenu.classList.toggle('header__navigation--open');
+}
